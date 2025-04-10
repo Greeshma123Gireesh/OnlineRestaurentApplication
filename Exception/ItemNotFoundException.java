a63f1f57c6bd094825a1d7857f9c2a43ce242dca
Greeshma Gireesh <greeshmagireesh1812@gmailcom>
2025-04-10 19:43:58 +0530

    removed

--- Exception/ItemNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.Restaurent.RestaurentOnlineService.Exception;
-
-public class ItemNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L; // Add serialVersionUID
-
-    public ItemNotFoundException(Long id) {
-        super("Could not find item with ID " + id);
-    }
-}
